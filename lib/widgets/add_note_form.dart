f8ecb3a96d0e9045f4f08c673cb7b5e1a8aa8c8e
nice <@https://github.com/mahmoudkarieem>
2024-07-02 20:15:57 +0200

    trigger add note model

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_notes_cubits/cubit/add_notes_cubit.dart';
+import 'package:notes_app/models/notes_model.dart';
 
 import 'custom_bottom_feild.dart';
 import 'custom_text_feild.dart';
 
-class AddNoteForm extends StatefulWidget{
+class AddNoteForm extends StatefulWidget {
   const AddNoteForm({super.key});
 
   @override
@@ -11,26 +14,24 @@ class AddNoteForm extends StatefulWidget{
 }
 
 class _AddNoteFormState extends State<AddNoteForm> {
- final GlobalKey <FormState>formKey=GlobalKey();
-  AutovalidateMode autoValidateMode=AutovalidateMode.disabled;
- String ? title,supTitle;
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  String? title, supTitle;
   @override
   Widget build(BuildContext context) {
-    return  Form(
+    return Form(
       key: formKey,
       autovalidateMode: autoValidateMode,
-      child:  Column(
-
+      child: Column(
         children: [
           const SizedBox(
             height: 25,
-
           ),
           CustomTextFeild(
             hintText: "Title",
             maxLines: 1,
-            onSaved: (value){
-              title=value;
+            onSaved: (value) {
+              title = value;
             },
           ),
           const SizedBox(
@@ -39,8 +40,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
           CustomTextFeild(
             hintText: "Content",
             maxLines: 5,
-            onSaved: (value){
-             supTitle=value;
+            onSaved: (value) {
+              supTitle = value;
             },
           ),
           const SizedBox(
@@ -48,20 +49,22 @@ class _AddNoteFormState extends State<AddNoteForm> {
           ),
           CustomButtomFeild(
             data: "Add",
-            onTap: (
-
-                ){
-              if(formKey.currentState!.validate()){
-formKey.currentState!.save();
-              }else{
-               autoValidateMode=AutovalidateMode.always;
-               setState(() {
-
-               });
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                NoteModel notes = NoteModel(
+                    title: title!,
+                    supTitle: supTitle!,
+                    date: DateTime.now().toString(),
+                    color: Colors.blueAccent.value);
+                BlocProvider.of<AddNotesCubit>(context).addNotes(notes);
+              } else {
+                autoValidateMode = AutovalidateMode.always;
+                setState(() {});
               }
             },
-
-          ),const SizedBox(
+          ),
+          const SizedBox(
             height: 20,
           ),
         ],
